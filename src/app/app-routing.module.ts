9d720582db92098179b731272eff2be2762d8876
Caleb <5322420+calebnjw@users.noreply.github.com>
2023-07-25 16:40:59 +0800

    updated app title

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -41,6 +41,7 @@ const routes: Routes = [
       { path: "", redirectTo: "feed", pathMatch: "full" },
       {
         path: "feed",
+        title: "Not Threads",
         component: FeedComponent,
       },
       {
