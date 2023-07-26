2fa952301fa7b6c2fda670d467a664d52df52ca1
Caleb <5322420+calebnjw@users.noreply.github.com>
2023-07-26 11:01:23 +0800

    change backend url

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -8,7 +8,7 @@ export class PostService {
   http = inject(HttpClient);
   cookieService = inject(CookieService);
 
-  serverUrl: string = "https://notthreadsbackend.calebnjw.com/api/post/";
+  serverUrl: string = "https://mini-project-backend-production.up.railway.app/api/post/";
 
   createPost(postData: FormData): Observable<any> {
     const headers = new HttpHeaders().set("Accept", "application/json");
